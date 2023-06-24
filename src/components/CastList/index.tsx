53f4dd49a34187171396052bd127789f90985e37
MariaMagallon <maria.magallon@estudiantat.upc.edu>
2023-06-24 23:01:48 +0200

    avoid null

--- src/components/CastList/index.tsx
+++ src/components/CastList/index.tsx
@@ -4,14 +4,16 @@ import styles from "./CastList.module.css";
 type CastListProps = {
   cast: Cast[];
 };
+const profileAvatar= "../assets/placeholder.png";
 
 function CastList(props: CastListProps) {
   const { cast } = props;
+  
   return (
     <div className={styles.cast}>
       {cast.map((item) => (
         <div key={item.id} className={styles.item} >
-          <img className={styles.itemImg} src={getImg(item.profile_path)} alt={item.name} />
+          <img className={styles.itemImg} src={item.profile_path ? getImg(item.profile_path): getImg(profileAvatar) } alt={item.name} />
           <p className={styles.name}>{item.name}</p>
         </div>
       ))}
